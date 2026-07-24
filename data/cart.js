25d48756af288252c733bcbed6aeebd1e7c89049
Ghulam Mustafa <mustafafa241122@gmail.com>
2026-07-24 21:29:14 +1300

    just practice

--- data/cart.js
+++ data/cart.js
@@ -6,3 +6,4 @@ console.log("hello world 2");
 console.log("back to change");
 console.log("github updation");
 console.log("changing in clone");
+console.log("for practice");
--- scrpts.js
+++ scrpts.js
@@ -2,3 +2,4 @@ console.log("version3");
 console.log("version3");
 console.log("github updation");
 console.log("changing in clone");
+console.log("for practice");
